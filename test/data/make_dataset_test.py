1e3153b8712594fbd0c86124e4bc36ecf5f5c460
Joaquin Blanco <blancojoaquin77@gmail.com>
2024-04-04 12:18:14 -0300

    cree un test para evaluar si existe las credenciales de kaggle

--- test/data/make_dataset_test.py
+++ test/data/make_dataset_test.py
@@ -2,6 +2,13 @@ from pathlib import Path
 import subprocess as sp
 
 
+def test_kaggle_json_exist():
+    home = Path.home()
+    kaggle_cred = home / ".kaggle" / "kaggle.json"
+    kaggle_cred.resolve()
+    assert kaggle_cred.is_file()
+
+
 def test_data_exists_after_run_script_for_download():
     train = Path("data/raw/train.csv")
     train.resolve()
