67018f62b85f596bf84aded919a78ee5af6c7f94
Joaquin Blanco <blancojoaquin77@gmail.com>
2024-04-03 23:41:28 -0300

    eliminando archivos para revisar dependencias del proyectos

--- test/data/make_dataset_test.py
+++ test/data/make_dataset_test.py
@@ -2,7 +2,7 @@ from pathlib import Path
 import subprocess as sp
 
 
-def test_data_visu_exists_after_run_script():
+def test_data_exists_after_run_script_for_download():
     train = Path("data/raw/train.csv")
     train.resolve()
     sp.run(args=["make", "data"])
